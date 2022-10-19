95294c794b7d9a60ebbd5317b537641277f3c921
haileyesus2433 <haileyesusayanaw1@gmail.com>
2022-10-19 15:46:49 +0300

    structure

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { CssBaseline } from '@mui/material';
 
-const App = () => (
-  <div>App</div>
-);
+import { NavBar, Home, MovieInformation, Profile, Actors } from './index';
+import useStyles from './styles';
+
+const App = () => {
+  const classes = useStyles();
+  return (
+
+    <div
+      className={classes.root}
+    >
+      <CssBaseline />
+      <NavBar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/movie/:id" element={<MovieInformation />} />
+          <Route exact path="/actors/:id" element={<Actors />} />
+          <Route exact path="/profile/:id" element={<Profile />} />
+        </Routes>
+      </main>
+    </div>
+  );
+};
 
 export default App;
